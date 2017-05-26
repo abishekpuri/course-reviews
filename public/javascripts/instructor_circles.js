336d6d00a9becb09f84aeecb469064fdc3084f15
Abishek Puri <apuri@connect.ust.hk>
2017-05-27 07:07:57 +0800

    Added callers for comments. TODO - Allow for multiple ways to layout circles, find similar professors, do course based visualization,text analysis

--- public/javascripts/instructor_circles.js
+++ public/javascripts/instructor_circles.js
@@ -1,11 +1,11 @@
 
 function makeInstructorCircles(instructors,averagetype,exp) {
   width = screen.width*100;
-  height = screen.height/3;
+  height = screen.height/5;
   var linearScale = d3.scale.pow()
           .exponent(exp)
           .domain([0, 5])
-          .range(["#11CBD7", "#F60C86"]);
+          .range(["red", "green"]);
    var svgContainer = d3.select("#circles").append("svg")
                                        .attr("width", width)
                                        .attr("height", height)
@@ -14,12 +14,15 @@ function makeInstructorCircles(instructors,averagetype,exp) {
                              .data(instructors)
                              .enter()
                            .append("circle")
-  currentX = 0;
-  currentY = screen.height/6;
+  currentX = 20;
+  prevradius = 0;
+  currentY = screen.height/10;
   var circleAttributes = circles
-                        .attr("cx", function (d) { currentX += d.reviews*2 + 15; return currentX; })
+                        .attr("cx", function (d) {currentX += prevradius*2 + 20;
+                           prevradius=d.reviews;
+                           return currentX; })
                         .attr("cy", function (d) { currentY += 0; return currentY; })
-                        .attr("r", function (d) { return d.reviews*134/134; })
+                        .attr("r", function (d) { return d.reviews; })
                         .style("fill", function(d) {
                           var avg = 0.0;
                           if(averagetype == 1) {
@@ -39,7 +42,9 @@ function makeInstructorCircles(instructors,averagetype,exp) {
                            else {
                              avg = (10*4.066 + (parseFloat(d.content) + parseFloat(d.grading) + parseFloat(d.workload) + parseFloat(d.teaching)))/14
                            }
-                           document.getElementById('name').innerHTML=d.name+': ';
+                           document.getElementById('name').innerHTML=d.name+': <br>';
+                           document.getElementById('id').innerHTML=d.instructorid;
+                           $('#id').hide();
                            document.getElementById('reviews').innerHTML=d.reviews+' Reviews';
                            document.getElementById('otherinfo').innerHTML="Content: "+d.content+"<br> Grading: "+d.grading+"<br> Workload: "+d.workload+"<br> Teaching: "+d.teaching;
                            document.getElementById('overall').innerHTML="Overall Average: "+avg.toPrecision(5);
@@ -57,16 +62,79 @@ function changeexponent(instructors,averagetype,exponent) {
   d3.select("svg").remove();
   makeInstructorCircles(instructors,averagetype,exponent);
 }
-function getcontent(id) {
+function getcontent() {
   $.post('/instructor/content',{
-    'id': id},
+    'id': $("#id").text()},
     function(result) {
       $("#content").empty();
-      $("#content").before("<h3> Content Comments </h3>")
+      if($("#banner").length) {
+          $("#banner").empty();
+          $("#banner").text("Content Comments");
+      }
+      else {
+        $("#content").before("<h3 id='banner'> Content Comments </h3>")
+      }
       for(i = 0; i < result.length;i++) {
         $('#content').append("<li> Rating:"+result[i].ratingcontent+
         " Course :" + result[i].subject+result[i].code+
         "<br>"+result[i].commentcontent+"</li>");
+      }
+    });
+}
+function getgrading() {
+  $.post('/instructor/grading',{
+    'id': $("#id").text()},
+    function(result) {
+      $("#content").empty();
+      if($("#banner").length) {
+          $("#banner").empty();
+          $("#banner").text("Grading Comments");
+      }
+      else {
+        $("#content").before("<h3 id='banner'> Grading Comments </h3>")
+      }
+      for(i = 0; i < result.length;i++) {
+        $('#content').append("<li> Rating:"+result[i].ratinggrading+
+        " Course :" + result[i].subject+result[i].code+
+        "<br>"+result[i].commentgrading+"</li>");
+      }
+    });
+}
+function getteaching() {
+  $.post('/instructor/teaching',{
+    'id': $("#id").text()},
+    function(result) {
+      $("#content").empty();
+      if($("#banner").length) {
+          $("#banner").empty();
+          $("#banner").text("Teaching Comments");
+      }
+      else {
+        $("#content").before("<h3 id='banner'> Teaching Comments </h3>")
+      }
+      for(i = 0; i < result.length;i++) {
+        $('#content').append("<li> Rating:"+result[i].ratingteaching+
+        " Course :" + result[i].subject+result[i].code+
+        "<br>"+result[i].commentteaching+"</li>");
+      }
+    });
+}
+function getworkload() {
+  $.post('/instructor/workload',{
+    'id': $("#id").text()},
+    function(result) {
+      $("#content").empty();
+      if($("#banner").length) {
+          $("#banner").empty();
+          $("#banner").text("Workload Comments");
+      }
+      else {
+        $("#content").before("<h3 id='banner'> Workload Comments </h3>")
+      }
+      for(i = 0; i < result.length;i++) {
+        $('#content').append("<li> Rating:"+result[i].ratingworkload+
+        " Course :" + result[i].subject+result[i].code+
+        "<br>"+result[i].commentworkload+"</li>");
       }
     });
 }
--- routes/instructor.js
+++ routes/instructor.js
@@ -14,5 +14,35 @@ router.post('/content', function(req, res, next) {
            res.send("ERROR: " + error);
          });
 });
+router.post('/grading', function(req, res, next) {
+  db.any("select review.commentgrading,review.ratinggrading,course.subject,course.code from review,course "+
+  "where course.id = review.courseid and instructorid=$1 " +
+  "order by review.ratinggrading desc",[req.body.id])
+         .then(function(result) {
+           res.send(result);
+         }).catch(function(error) {
+           res.send("ERROR: " + error);
+         });
+});
+router.post('/teaching', function(req, res, next) {
+  db.any("select review.commentteaching,review.ratingteaching,course.subject,course.code from review,course "+
+  "where course.id = review.courseid and instructorid=$1 " +
+  "order by review.ratingteaching desc",[req.body.id])
+         .then(function(result) {
+           res.send(result);
+         }).catch(function(error) {
+           res.send("ERROR: " + error);
+         });
+});
+router.post('/workload', function(req, res, next) {
+  db.any("select review.commentworkload,review.ratingworkload,course.subject,course.code from review,course "+
+  "where course.id = review.courseid and instructorid=$1 " +
+  "order by review.ratingworkload desc",[req.body.id])
+         .then(function(result) {
+           res.send(result);
+         }).catch(function(error) {
+           res.send("ERROR: " + error);
+         });
+});
 
 module.exports = router;
